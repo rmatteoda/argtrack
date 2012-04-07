07bfc0c0e2411b043e24f25453e140e55f827129
rmatteoda <rmatteoda@gmail.com>
2012-04-07 11:38:22 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Argtrack::Application.config.secret_token = 'e2f0867f3b4cc8582e1a2dba4b7f49bfd16f4f9f8f50b5d83eb746abad705b6dd1b336c8f6ac53bdfb315293b553b81e5e04d39e5d851bd871ece68b0e4a275a'
